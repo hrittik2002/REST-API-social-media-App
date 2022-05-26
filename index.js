bee5ddbab2bd06f1d80527f1c7b8639f1b4680fb
Hrittik <hrittikgarain@gmail.com>
2022-05-26 19:50:17 +0530

    c1 : basic setup done

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+dotenv.config();
+
+/** CONNECTING TO MONGODB **/
+mongoose.connect(process.env.MONGO_URL)
+const db = mongoose.connection;
+db.on("error" , console.error.bind(console , "connection error:"))
+db.once("open" , () =>{
+    console.log("Database connected")
+});
+
+/** MIDDLEWARES **/
+app.use(express.json())
+app.use(helmet());
+app.use(morgan("common"))
+
+/** ROUTES **/
+app.get("/", (req ,res)=>{
+    res.send("welcome to homepage")
+})
+
+app.listen(8000 , ()=>{
+    console.log("Backend server is running!");
+})
